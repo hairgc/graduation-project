475439915d552c239d5e10be441e29a8fc3d9fda
mahui53541 <mahuiking9527@gmail.com>
2018-04-29 19:43:02 +0800

    生成Dao

---
+++ java-project/src/main/java/com/github/mahui53541/graduation/GraduationApplication.java
@@ -0,0 +1,12 @@
+package com.github.mahui53541.graduation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduationApplication.class, args);
+	}
+}
---
+++ java-project/src/main/java/com/github/mahui53541/graduation/model/User.java
@@ -0,0 +1,63 @@
+package com.github.mahui53541.graduation.model;
+
+public class User {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private Boolean sex;
+
+    private String phoneNumber;
+
+    private String nickname;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Boolean getSex() {
+        return sex;
+    }
+
+    public void setSex(Boolean sex) {
+        this.sex = sex;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber == null ? null : phoneNumber.trim();
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname == null ? null : nickname.trim();
+    }
+}
